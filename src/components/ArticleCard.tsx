667675f2a806f9d0a46e7465a689aa153bc9f8d5
waleedHAlmenawy <waleed.almenawy@outlook.com>
2024-04-18 17:43:50 +0200

    fix date in the home card

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import ArticleDetails from "./popups/ArticleDetails";
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 export default function ArticleCard({ article, handleDeleteArticle }: any) {
   const [popupTrigger, setPopupTrigger] = useState(false);
@@ -12,7 +14,7 @@ export default function ArticleCard({ article, handleDeleteArticle }: any) {
           setPopupTrigger(true);
         }}
       >
-        <img src={article.image} alt="article-img" className="col-span-1 hover:scale-125 transition-all" style={
+        <img src={article.image || <Skeleton />} alt="article-img" className="col-span-1 hover:scale-125 transition-all" style={
           {
             height: '300px',
             width: '400px',
--- src/hooks/useHomeArticles.tsx
+++ src/hooks/useHomeArticles.tsx
@@ -13,6 +13,23 @@ const useHomeArticles = () => {
       if (!homeArticles[0]) {
         try {
           const req = await axios.get(ARTICLE_URL);
+
+          const articles = req.data.map((article: any) => {
+            let date = new Date(+article.date);
+
+            let day = `${date.getDay()}`
+            let month = `${date.getMonth()}`
+            let year = `${date.getFullYear()}`
+
+            if (+day < 10) day = "0" + day;
+            if (+month < 10) month = "0" + month;
+
+            article.date = `${day}/${month}/${year}`;
+
+            return article;
+          })
+
+          console.log(articles);
           setHomeArticles(await req.data);
         } catch (err: any) {
           if (!err.response) {
